30bc855feba237b90052b84c41fbdbe4e227c540
lahlaliwassim2 <wlahlali34@gmail.com>
2022-12-18 13:42:16 +0100

    ajouter la structure et le composants initial

---
+++ cartshop/src/App.js
@@ -0,0 +1,22 @@
+import { Container } from "react-bootstrap";
+import {Routes , Route } from 'react-router-dom'
+import Home from './components/Home'
+import Store from './components/Store'
+import About from './components/About'
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+   <>
+   <Navbar />
+   <Container className="mb-4 <">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+   </Container>
+   </>
+  );
+}
+
+export default App;
